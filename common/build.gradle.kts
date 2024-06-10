e2800ad006aebdc6aea82a50cfb9b1127101b160
Rafa <32463720+Rafacasari@users.noreply.github.com>
2024-06-09 21:27:35 -0300

    Added graalvm so GitHub can compile

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,13 +11,11 @@ architectury {
 dependencies {
     minecraft("com.mojang:minecraft:${property("minecraft_version")}")
     mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
-    modCompileOnly("com.cobblemon:mod:${property("cobblemon_version")}")
 
+    modCompileOnly("org.graalvm.js:js:22.3.0")
+    modCompileOnly("com.cobblemon:mod:${property("cobblemon_version")}")
 
     // alL fabric dependencies:
     modCompileOnly("net.fabricmc:fabric-loader:${property("fabric_loader_version")}")
     modCompileOnly("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
-    //modCompileOnly("dev.architectury:architectury-fabric:${property("architectury_version")}")
-
-
 }
